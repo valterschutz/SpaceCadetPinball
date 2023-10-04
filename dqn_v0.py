36d39778524b6c98a54de3de46ac28cd6f2628ef
eliasstenhede <elias.stenhede@gmail.com>
2023-10-04 19:31:52 +0000

    variable epsilon and save models

--- dqn_v0.py
+++ dqn_v0.py
@@ -1,4 +1,5 @@
 import torch
+from datetime import datetime
 import torch.nn as nn
 import torch.optim as optim
 import random
@@ -27,9 +28,15 @@ class DQN(nn.Module):
     def forward(self, x):
         return self.layers(x)
 
+def save_dqn_model(dqn):
+    current_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
+    model_path = f"./dqn_models/model{current_datetime}.pt"
+    torch.save(dqn.state_dict(), model_path)
+
 NUM_ACTIONS = 7 # 11 with table bump, 7 without
 GAMMA = 0.995
-EPSILON = 1
+EPSILON_START = 1
+EPSILON_END = 0.1
 REPLAY_BUFFER_SIZE = 1000  # Size of the replay buffer
 BATCH_SIZE = 32  # Batch size for training
 
@@ -56,11 +63,17 @@ def main():
     replay_buffer = ReplayBuffer(REPLAY_BUFFER_SIZE)
 
     # Training loop
-    num_episodes = 1000  # Number of episodes to train
-    for episode in range(num_episodes):
+    num_episodes = 10000  # Number of episodes to train
+    for episode in range(1, num_episodes):
+        current_time = datetime.now()
+        formatted_time = current_time.strftime("%d %b %H:%M:%S")
+        if episode % 100 == 0:
+            print(f"{formatted_time} Saving model")
+            save_dqn_model(dqn)
+        EPSILON = EPSILON_START * (1 - episode/num_episodes) + episode/num_episodes * EPSILON_END 
         env = GameEnvironment(600, 416)
         state = env.get_external_state().to(get_device())
-        print(f"   Training episode {episode}...")
+        print(f"{formatted_time} Training episode {episode}/{num_episodes} epsilon={EPSILON:.3f}")
         while True:
             if random.random() < EPSILON:
                 action = random.randint(0, NUM_ACTIONS - 1)  # Explore
@@ -72,44 +85,39 @@ def main():
             next_state, reward = env.step(action)
             next_state = next_state.to(get_device())
             reward = reward.to(get_device())
-            # if reward.item():
-            #     print(f"      Score updated to {env.score}")
-                #last_layer_weights = list(dqn.parameters())[-2].data
-                #print("Part of last layer in dqn:", last_layer_weights[:,0].detach().cpu().numpy())
-                #print("Q values: [R r L l ! . p]", q_values.detach().cpu().numpy())
 
-            # Store the transition in the replay buffer
-            # if env.frame_id > 500: # We don't need to save the first 300 frames. The ball hasn't dropped yet.
-            #     replay_buffer.push((state, action, next_state, reward))
+            #Store the transition in the replay buffer
+            replay_buffer.push((state, action, next_state, reward))
 
             # Sample a batch of experiences from the replay buffer
-            # if len(replay_buffer.buffer) >= BATCH_SIZE:
-            #     batch = replay_buffer.sample(BATCH_SIZE)
-            #
-            #     states, actions, next_states, rewards = zip(*batch)
-            #
-            #     states = torch.stack(states)
-            #     actions = torch.tensor(actions, dtype=torch.long).unsqueeze(1).to(get_device())
-            #     next_states = torch.stack(next_states)
-            #     rewards = torch.tensor(rewards, dtype=torch.float32).unsqueeze(1).to(get_device())
-            #
-            #     optimizer.zero_grad()
-            #
-            #     # Compute the Q-values and target values
-            #     q_values = dqn(states)
-            #     next_q_values = dqn(next_states)
-            #     max_next_q_values = torch.max(next_q_values, dim=1, keepdim=True)[0]
-            #     targets = rewards + GAMMA * max_next_q_values
-            #
-            #     # Get the Q-values for the selected actions
-            #     # what the f*** does this do?
-            #     q_values = q_values.gather(1, actions)
-            #
-            #     # Compute the loss and update the Q-network
-            #     loss = criterion(q_values, targets)
-            #     #print(f"Loss: {loss.item():.3f}")
-            #     loss.backward()
-            #     optimizer.step()
+            if len(replay_buffer.buffer) >= BATCH_SIZE*10:
+                #print(len(replay_buffer.buffer))
+                batch = replay_buffer.sample(BATCH_SIZE)
+        
+                states, actions, next_states, rewards = zip(*batch)
+            
+                states = torch.stack(states)
+                actions = torch.tensor(actions, dtype=torch.long).unsqueeze(1).to(get_device())
+                next_states = torch.stack(next_states)
+                rewards = torch.tensor(rewards, dtype=torch.float32).unsqueeze(1).to(get_device())
+            
+                optimizer.zero_grad()
+            
+                # Compute the Q-values and target values
+                q_values = dqn(states)
+                next_q_values = dqn(next_states)
+                max_next_q_values = torch.max(next_q_values, dim=1, keepdim=True)[0]
+                targets = rewards + GAMMA * max_next_q_values
+            
+                # Get the Q-values for the selected actions
+                # what the f*** does this do?
+                q_values = q_values.gather(1, actions)
+            
+                # Compute the loss and update the Q-network
+                loss = criterion(q_values, targets)
+                #print(f"Loss: {loss.item():.3f}")
+                loss.backward()
+                optimizer.step()
 
             state = next_state
             if env.is_done():
@@ -120,84 +128,3 @@ if __name__ == "__main__":
     main()
 
 
-"""
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import random
-import time
-from train_cnn import load_latest_model, get_device
-from gamehandler import GameEnvironment
-
-
-# Define the DQN model
-class DQN(nn.Module):
-    def __init__(self, NUM_ACTIONS):
-        super(DQN, self).__init__()
-        
-        # Load the BallDetectionCNN model
-        self.ball_cnn = load_latest_model()
-        for param in self.ball_cnn.parameters():
-            param.requires_grad = False
-        
-        # Define the Q-network layers
-        self.layers = nn.Sequential(
-                self.ball_cnn,
-                nn.Linear(128, 64),
-                nn.ReLU(),
-                nn.Linear(64, NUM_ACTIONS),
-                )
-        
-    def forward(self, x):
-        return self.layers(x)
-
-NUM_ACTIONS = 7
-GAMMA = 0.995
-EPSILON = 0.1
-
-def main():
-    dqn = DQN(NUM_ACTIONS)
-    dqn.to(get_device())
-
-    optimizer = optim.Adam(dqn.parameters(), lr=0.001)
-    criterion = nn.MSELoss()
-
-    # Training loop
-    num_episodes = 1000  # Number of episodes to train
-    for episode in range(num_episodes):
-        env = GameEnvironment(600, 416)
-        state = env.get_external_state().to(get_device())
-        print(f"   Training episode {episode}...")
-        while True:
-            if random.random() < EPSILON:
-                action = random.randint(0, NUM_ACTIONS - 1)  # Explore
-            else:
-                q_values = dqn(state.unsqueeze(0))
-                action = torch.argmax(q_values).item()  # Exploit
-            
-            # Simulate the game step and get the next state and reward
-            next_state, reward = env.step(action)
-            next_state = next_state.to(get_device())
-            reward = reward.to(get_device())
-            if reward.item():
-                print(f"      Score updated to {env.score}")
-            
-            # Update the Q-network using the Q-learning formula
-            target = reward + 0.99 * torch.max(dqn(next_state.unsqueeze(0)))
-            current = dqn(state.unsqueeze(0))[0][action]
-            loss = criterion(current, target)
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            state = next_state
-            if env.is_done():
-                del env
-                break
-
-
-
-
-main()
-"""

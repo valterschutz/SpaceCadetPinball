3d203ac2dd90268f3b569d015f060d8f16fc8468
Valter Schutz <valterschutz@protonmail.com>
2023-10-19 11:47:06 +0200

    perf plotter

---
+++ perf_plotter.py
@@ -0,0 +1,25 @@
+import sys
+import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+model_name = sys.argv[1]
+
+with open(f"pickles/perf_{model_name}.pkl", "rb") as file:
+    scores = pickle.load(file)
+
+scores = [score for score in scores if score > 10]
+episodes = list(range(1,len(scores)+1))
+
+fig, axes = plt.subplots(1,2)
+sns.lineplot(x=episodes, y=scores, ax=axes[0])
+axes[1].axvspan(np.log10(2_500_000), np.log10(3_700_000), alpha=0.5, color='gray')
+axes[0].set_xlabel("Episode")
+axes[0].set_ylabel("Score")
+axes[0].set_title(f"Mean score: {sum(scores)/len(scores)}")
+sns.histplot(np.log10(scores), kde=True, ax=axes[1])
+axes[1].set_xlabel("log10(score)")
+axes[1].set_ylabel("Frequency")
+axes[1].set_title("Score histogram")
+plt.show()
